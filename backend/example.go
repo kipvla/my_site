4220cd6986033c1ec26bdd6660d911fe4b722155
Kip Riecken <kip.riecken@gmail.com>
2020-12-14 17:47:46 -0500

    connect to db and begin work on passing data

--- backend/example.go
+++ backend/example.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// "github.com/gin-gonic/gin"
-	// "net/http"
+	"github.com/gin-gonic/gin"
+	"net/http"
 	"fmt"
 	"os"
 	"github.com/jackc/pgx/v4"
@@ -10,8 +10,7 @@ import (
 )
 
 func main() {
-	os.Setenv("DATABASE_URL", "postgresql://localhost:8080")
-	os.Setenv("database", "blogs")
+	os.Setenv("DATABASE_URL", "postgresql://localhost/postgres")
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
@@ -19,43 +18,51 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	var title string
+	err = conn.QueryRow(context.Background(), "select title from blogs").Scan(&title)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(greeting)
+	fmt.Println(title)
 
-	// r := gin.Default()
-	// // r.LoadHTMLGlob("../public/templates/*")
+	r := gin.Default()
+	r.LoadHTMLGlob("../public/index.html")
+	r.Static("build", "../public/build")
+	r.Static("images", "../public/images")
+	r.Static("sounds", "../public/sounds")
+	r.StaticFile("favicon-16x16.png", "../public/favicon-16x16.png")
+	r.StaticFile("favicon-32x32.png", "../public/favicon-32x32.png")
+	r.StaticFile("site.webmanifest", "../public/site.webmanifest")
+	r.StaticFile("global.css", "../public/global.css")
 
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	// r.GET("/user/:name", func(c *gin.Context) {
-	// 	name := c.Param("name")
-	// 	c.String(http.StatusOK, "Hello %s", name)
-	// })
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
 
-	// r.GET("/index", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-	// 		"title": "Main website",
-	// 	})
-	// })
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+			"subtitle": title,
+		})
+	})
 
-	// r.POST("/post", func(c *gin.Context) {
-	// 	id := c.Query("id")
-	// 	page := c.DefaultQuery("page", "0")
-	// 	name := c.PostForm("name")
-	// 	message := c.PostForm("message")
+	r.POST("/post", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
 
-	// 	fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
-	// })
+		fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+	})
 
-	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- src/components/Blog/BlogPost.svelte
+++ src/components/Blog/BlogPost.svelte
@@ -4,9 +4,22 @@
     export let time = "4:00 PM";
     export let image = "images/KR11.jpg";
     export let body = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ducimus distinctio necessitatibus quo repellendus sed explicabo eveniet, omnis blanditiis reiciendis amet perferendis, quod minima consequatur dolores eius, cumque repudiandae praesentium?";
+    const response = fetch("http://localhost:8080").then( resp => console.log(resp.JSON)).then(data => console.log(data));
+    const fetchImage = (async () => {
+		const response = await fetch("http://localhost:8080").then( resp => console.log(resp.JSON)).then(data => console.log(data))
+    return await response.json()
+	})()
 </script>
 
-<div class="container">
+{#await fetchImage}
+	<p>...waiting</p>
+{:then data}
+	<!-- svelte-ignore a11y-img-redundant-alt -->
+	<h1>{data.message}</h1>
+{:catch error}
+	<p>An error occurred!</p>
+{/await}
+<div class="container" on:click={response}>
     <div class="col col-lg-2"></div>
     <div class="col col-lg-8 blog-post">
         <h2>{title}</h2>
