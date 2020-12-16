ff82838c8acc507d084d6e00fed0f3398472fb47
Kip Riecken <kip.riecken@gmail.com>
2020-12-16 12:48:48 -0500

    work on backend, trying to get more than one row from a query

--- backend/example.go
+++ backend/example.go
@@ -8,6 +8,9 @@ import (
 	"github.com/jackc/pgx/v4"
 	"context"
 	"time"
+	// "strings"
+	// "github.com/jackc/pgproto3/v2"
+	"log"
 )
 
 func main() {
@@ -30,7 +33,91 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(title)
+	ids := make([]string, 0)
+	rows, err := conn.Query(context.Background(), "select url from images")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(rows)
+	defer rows.Close()
+	for rows.Next() {
+		var id string
+		if err := rows.Scan(&id); err != nil {
+			// Check for a scan error.
+			// Query rows will be closed with defer.
+			fmt.Println(err)
+			log.Fatal(err)
+		}
+		ids = append(ids, id)
+		fmt.Println(id)
+	}
+	fmt.Println(ids)
+
+	// urls := make([]string, 0)
+	// rows, err := conn.Query(context.Background(), "SELECT url FROM images")
+
+	// var url string
+	// if err := rows.Scan(&url); err != nil {
+	// 	// Check for a scan error.
+	// 	// Query rows will be closed with defer.
+	// 	fmt.Println(err)
+	// }
+	// urls = append(urls, url)
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(urls[0])
+	// }
+
+	// fmt.Println(urls)
+
+	// if err != nil {
+	// 	return err
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	err = rows.Scan(&urls)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	err = f(rows)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// }
+
+	// if err := rows.Err(); err != nil {
+	// 	return nil, err
+	// }
+
+	// test code
+
+	// rows, err := conn.QueryContext(context.Background(), "SELECT url FROM images")
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	// }
+	// defer rows.Close()
+	// urls := make([]string, 0)
+
+	// for rows.Next() {
+	// 	var url string
+	// 	if err := rows.Scan(&url); err != nil {
+	// 		// Check for a scan error.
+	// 		// Query rows will be closed with defer.
+	// 		fmt.Fprintf(os.Stderr, "Scan error: %v\n", err)
+	// 	}
+	// 	urls = append(urls, url)
+	// }
+
+	// // Rows.Err will report the last error encountered by Rows.Scan.
+	// if err := rows.Err(); err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Rows.Scan failed: %v\n", err)
+	// }
+	// fmt.Printf("%s are years old", strings.Join(urls, ", "))
+
+	// end of test code
 
 	r := gin.Default()
 	r.LoadHTMLGlob("../public/index.html")
@@ -41,6 +128,10 @@ func main() {
 	r.StaticFile("favicon-32x32.png", "../public/favicon-32x32.png")
 	r.StaticFile("site.webmanifest", "../public/site.webmanifest")
 	r.StaticFile("global.css", "../public/global.css")
+	r.StaticFile("favicon.ico", "../public/favicon.ico")
+	r.StaticFile("apple-touch-icon.png", "../public/apple-touch-icon.png")
+	r.StaticFile("android-chrome-192x192.png", "../public/android-chrome-192x192.png")
+	r.StaticFile("android-chrome-512x512.png", "../public/android-chrome-512x512.png")
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
