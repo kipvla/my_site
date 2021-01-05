f046c269e662b4acfb60c3b19a208ba77873ee44
Kip Riecken <kip.riecken@gmail.com>
2021-01-05 16:52:26 -0500

    fix image string case in schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS blogs;
 
 CREATE TABLE blogs (id serial primary key, title VARCHAR(50) not null, body text not null, date timestamp not null, image text not null);
 
-INSERT INTO blogs (title, body, date, image) VALUES ('First Post', 'This is my first post. Yay!', '2020-12-12 22:43:12.463406', 'images/kr10.jpg');
-INSERT INTO blogs (title, body, date, image) VALUES ('Time for #2', 'Something unique!', '2020-12-16 17:07:30.055948 ', 'images/kr11.jpg');
+INSERT INTO blogs (title, body, date, image) VALUES ('First Post', 'This is my first post. Yay!', '2020-12-12 22:43:12.463406', 'images/KR10.jpg');
+INSERT INTO blogs (title, body, date, image) VALUES ('Time for #2', 'Something unique!', '2020-12-16 17:07:30.055948 ', 'images/KR11.jpg');
