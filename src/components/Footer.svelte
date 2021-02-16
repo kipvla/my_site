542426c54756fe594fe5ba27967ef2f5354deaab
Kip Riecken <kip.riecken@gmail.com>
2021-02-15 20:13:02 -0500

    change size of social icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,18 @@
 <div class="container pt-3">
   <div class="footer">
-    <div class="social">
-      <a href="https://www.instagram.com"><i
-          class="nav-link fab fa-instagram" /></a>
-      <a href="https://www.facebook.com"><i
-          class="nav-link fab fa-facebook-square" /></a>
-      <a href="https://www.twitter.com"><i class="nav-link fab fa-youtube" /></a>
+    <div class="social my-3">
+      <a href="https://www.instagram.com"
+        ><i class="mx-3 fab fa-instagram" alt="instagram" /></a
+      >
+      <a href="https://www.facebook.com"
+        ><i class="mx-3 fab fa-facebook-square" alt="facebook" /></a
+      >
+      <a href="https://www.youtube.com"
+        ><i class="mx-3 fab fa-youtube" alt="youtube" /></a
+      >
     </div>
     &copy; Copyright 2021 Kip Riecken
     <br />
     Proudly self-designed.
   </div>
 </div>
-
