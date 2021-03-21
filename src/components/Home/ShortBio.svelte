e5147f659e3f3d7f39e1a1ece8d76d4ef67ed1af
Kip Riecken <kip.riecken@gmail.com>
2021-03-20 21:28:49 -0400

    update short bio

--- src/components/Home/ShortBio.svelte
+++ src/components/Home/ShortBio.svelte
@@ -1,19 +1,15 @@
 <script>
-  import {link} from 'svelte-routing';
+  import { link } from "svelte-routing";
 </script>
 
 <div class="short-bio py-3">
-  <div class="container w-100 text-justify px-5 d-flex flex-column align-items-center">
+  <div
+    class="container w-100 text-justify px-5 d-flex flex-column align-items-center"
+  >
     <p class="py-5">
-      Hi, I’m Kip. That’s a viola. I have been playing music for almost 30 years, and teaching for over 15. But I like to think there's more to me. 
-      <!-- A violist and an educator, Kip Riecken has performed with the Chicago and
-      St. Louis Symphonies, the Civic Orchestra of Chicago, and alongside Yo-Yo Ma
-      and Tabea Zimmerman. Kip's study of the Alexander Technique, an
-      indispensable method for enhancing efficiency of movement and personal
-      expression, inspires a lively and warm perspective on music-making and
-      teaching. Click <a href="https://www.youtube.com/channel/UCOzQuf-qTzKJx6YfYTaJLpQ?view_as=subscriber">here</a> to see Kip talk about the Alexander Technique on
-      YouTube. -->
+      Hi, I’m Kip. Welcome! That’s a viola. I am a longtime musician, but have
+      so many interests. Click below to find out more!
     </p>
-    <p class="text-dark"><a class="btn" href="/about" use:link>Want to find out?</a></p>
+    <p class="text-dark"><a class="btn" href="/about" use:link>About me</a></p>
   </div>
 </div>
